797faf62ea4fb9c5258223a33e93e76c72dc27cc
Shamim Ahmed <shamim.buet.99@gmail.com>
2020-08-04 23:20:39 +1000

    Implement a generic stack

---
+++ cormen-ch10/src/main/java/edu/buet/cse/ds/CustomStackImpl.java
@@ -0,0 +1,77 @@
+package edu.buet.cse.ds;
+
+public class CustomStackImpl<T> implements CustomStack<T> {
+  private static final int DEFAULT_INITIAL_CAPACITY = 16;
+
+  private int count = -1;
+
+  private T[] itemArray;
+
+  public CustomStackImpl() {
+    this(DEFAULT_INITIAL_CAPACITY);
+  }
+
+  @SuppressWarnings("unchecked")
+  public CustomStackImpl(int initialCapacity) {
+    if (initialCapacity <= 0) {
+      throw new IllegalArgumentException("initialCapacity must be a positive integer");
+    }
+
+    itemArray = (T[]) new Object[initialCapacity];
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  public void push(T item) {
+    if (count == itemArray.length - 1) {
+      increaseCapacity();
+    }
+
+    itemArray[++count] = item;
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  public T pop() {
+    if (isEmpty()) {
+      throw new IllegalStateException("Stack is empty");
+    }
+
+    return itemArray[count--];
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  public T top() {
+    if (isEmpty()) {
+      throw new IllegalStateException("Stack is empty");
+    }
+
+    return itemArray[count];
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  public boolean isEmpty() {
+    return count < 0;
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  public int size() {
+    return count + 1;
+  }
+
+  private void increaseCapacity() {
+    int n = 2 * itemArray.length;
+    @SuppressWarnings("unchecked")
+    T[] expandedArray = (T[]) new Object[n];
+    System.arraycopy(itemArray, 0, expandedArray, 0, itemArray.length);
+    itemArray = expandedArray;
+  }
+}
