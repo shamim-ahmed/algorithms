79b7cd56069f22deea713777c8f613501414b40f
Shamim Ahmed <shamim.buet.99@gmail.com>
2020-08-09 19:38:39 +1000

    Minor modification

--- cormen-ch22/src/main/java/edu/buet/cse/ch22/bfs/BFSTrial.java
+++ cormen-ch22/src/main/java/edu/buet/cse/ch22/bfs/BFSTrial.java
@@ -5,9 +5,11 @@ import java.util.Scanner;
 import edu.buet.cse.ch22.Graph;
 
 public class BFSTrial {
+  private static final String RESOURCE_FILE = "/edu/buet/cse/ch22/bfs/breadth-first.txt";
+
   public static void main(String... args) {
-    try (Scanner scanner = new Scanner(
-        BreadthFirstSearch.class.getResourceAsStream("/edu/buet/cse/ch22/bfs/breadth-first.txt"))) {
+    try (Scanner scanner =
+        new Scanner(BreadthFirstSearch.class.getResourceAsStream(RESOURCE_FILE))) {
       final int n = Integer.parseInt(scanner.nextLine().trim());
       Graph graph = new Graph(n);
 
